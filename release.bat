ff3972d27d8aa970b6267e8e710a1ec060dd8a8d
DimaBenivo <dima@benivo.com>
2017-10-04 16:12:40 +0400

    automatic push

--- release.bat
+++ release.bat
@@ -2,7 +2,7 @@ git add *
 git commit -m "automatic push"
 git push
 ng build --prod
-Remove-Item ..\benivo.github.io\*
+del ..\benivo.github.io\*
 xcopy .\dist\* ..\benivo.github.io\*
 cd ..
 cd benivo.github.io
